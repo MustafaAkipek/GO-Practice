c3d608233f4c65fd89e46896a4487e1e8a1ed851
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-05-16 20:40:55 +0300

    practice

---
+++ go_essential/zero_values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name string // string --> ""
+	var age int // numeric --> 0
+	var isMarried bool // bool --> false
+
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(isMarried)
+}
