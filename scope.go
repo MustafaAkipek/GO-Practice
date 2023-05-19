b00ee3a7f2765e9edbcc2750ef6dc7b73d996b02
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-05-19 08:10:51 +0300

    scope

---
+++ scope.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+var packVar = "Package Scope"
+var funcVar = "Func(Package) Scope"
+
+func main() {
+
+	var funcVar = "Func Scope"
+
+	fmt.Println(funcVar)
+	fmt.Println(packVar)
+
+	myFunc()
+}
+
+func myFunc() {
+
+	fmt.Println(funcVar)
+}
