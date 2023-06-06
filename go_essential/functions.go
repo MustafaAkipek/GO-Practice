fd01596f0634fba0658b6b1f9f822aee051bb19d
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-06-06 15:59:26 +0300

    practice

--- go_essential/functions.go
+++ go_essential/functions.go
@@ -7,6 +7,9 @@ func main() {
 	fmt.Println(sum(5, 10))
 
 	hello("Mustafa", 19)
+
+	bolum , kalan := bolme(104, 5)
+	fmt.Println(bolum, kalan)
 }
 
 func sum(x int, y int) int {
@@ -15,4 +18,11 @@ func sum(x int, y int) int {
 
 func hello(name string, age float32) {
 	fmt.Println("Benim Adim", name, age, "yasindayim.")
+}
+
+func bolme(bolunen, bolen int) (bolum, kalan int) {
+	bolum = bolunen / bolen
+	kalan = bolunen % bolen
+	
+	return bolum, kalan
 }
